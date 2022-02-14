25f120c5c93e9f8bf7f294e860b88ebd1272ae39
Mohamad Krayem <mk2242682@gmail.com>
2022-02-14 14:46:58 +0200

    exercises 3.15-3.18

--- index.js
+++ index.js
@@ -24,23 +24,25 @@ app.get('/info/', (req, res)=>{
   res.send(`<p>Phonebook has info for 4 people</p><br /><p>${date}</p>`)
 })
 
-app.get('/api/persons/:id/', (req,res)=>{
-  Person.findById(req.params.id).then(person=>{
-    res.json(person)
-  })
+app.get('/api/persons/:id/', (req, res, next)=>{
+  Person.findById(req.params.id)
+    .then(person=>{
+      if (person) res.json(person);
+      else res.status(404).end();
+    })
+    .catch(error => { next(error) })
 })
 
-app.delete('/api/persons/:id/', (req, res)=>{
-  const id = Number(req.params.id);
-  persons = persons.filter(person=>id!==id);
-
-  res.status(204).end();
+app.delete('/api/persons/:id/', (req, res, next)=>{
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => { res.status(204).end() })
+    .catch(error => next(error))
 })
 
 
 app.post('/api/persons', (req, res)=>{
   const body = req.body;
-  
+
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -51,6 +53,28 @@ app.post('/api/persons', (req, res)=>{
   })
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if(error.name === 'CastError') return res.status(400).send({ error: 'malformatted id'});
+  
+  next(error);
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{console.log('app is running on '+ PORT)});
